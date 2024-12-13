107cd64d4c3681ff93dc323f81a8ee4780ab3ca8
Sandra Misigo <misigosandra@gmail.com>
2024-12-13 14:27:53 +0300

    phase-1-fetch-lab

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+// Fetch data from the Game of Thrones API and return the fetch promise
+return fetch('https://anapioficeandfire.com/api/books')
+.then(response => response.json()) // convert response to JSON
+.then(books => renderBooks(books)) // Pass the JSON data to renderBooks
+.catch(error => console.error('Error fetching books:', error)); // Handle potential errors
   
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main'); //selects the main element
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h2 = document.createElement('h2'); //creates a <h2> element
+    h2.innerHTML = book.name; //sets inner HTML to the book's name
+    main.appendChild(h2); //appends <h2> to the <main> element
   });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  fetchBooks();// Call fetchBooks when the DOM content is loaded
 });
